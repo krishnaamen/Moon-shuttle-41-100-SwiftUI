feb838944d2e22444d45b74511c37d29863e9aee
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-02-20 15:31:13 +0100

    Initial Commit

---
+++ Space-shuttle/Space_shuttleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_shuttleApp.swift
+//  Space-shuttle
+//
+//  Created by min phone on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Space_shuttleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
