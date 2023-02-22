dece44e60613d3f750118c13e80c50613ee06a33
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-02-22 12:52:05 +0100

    implementing the details view

---
+++ Space-shuttle/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Space-shuttle
+//
+//  Created by min phone on 20/02/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable,Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Space-shuttle/Mission.swift
@@ -0,0 +1,30 @@
+//
+//  Mission.swift
+//  Space-shuttle
+//
+//  Created by min phone on 22/02/2023.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole:Codable{
+        let name: String
+        let role: String
+    }
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String{
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
